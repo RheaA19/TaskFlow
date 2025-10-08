48f03868931cf3b2f53add6f4be83aadee77d803
jimariematuran8-droid <jimariematuran8@gmail.com>
2025-10-08 11:54:53 +0800

    Create Tasklist.js

---
+++ Tasklist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TaskListView = () => {
+  return (
+    <div className="container mt-3">
+      <h2>Task List</h2>
+      <p>This is where the list of tasks will be displayed.</p>
+    </div>
+  );
+};
+
+export default TaskListView;
