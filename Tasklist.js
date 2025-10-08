bbabf9f493b95e183b42dc46c03a3937d42ce5b9
jimariematuran8-droid <jimariematuran8@gmail.com>
2025-10-08 12:39:59 +0800

    Update Tasklist.js

--- Tasklist.js
+++ Tasklist.js
@@ -1,12 +1,32 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import TaskListView from './components/TaskListView';
+import AddTaskView from './components/AddTaskView';
 
-const TaskListView = () => {
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks(prevTasks => [...prevTasks, { ...task, id: Date.now() }]);
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
+  };
+
   return (
-    <div className="container mt-3">
-      <h2>Task List</h2>
-      <p>This is where the list of tasks will be displayed.</p>
-    </div>
+    <Router>
+      <Header />
+      <main className="container mt-3">
+        <Routes>
+          <Route path="/" element={<TaskListView tasks={tasks} deleteTask={deleteTask} />} />
+          <Route path="/add" element={<AddTaskView addTask={addTask} />} />
+        </Routes>
+      </main>
+    </Router>
   );
-};
+}
 
-export default TaskListView;
+export default App;
